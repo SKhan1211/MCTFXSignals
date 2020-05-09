afb43851dad3e058bc33047984004a4b7ab8d763
Suhaib <suhaib92khan@gmail.com>
2020-05-08 23:59:28 -0400

    Complete create users migration

--- db/migrate/20200509034400_create_users.rb
+++ db/migrate/20200509034400_create_users.rb
@@ -2,15 +2,17 @@ class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
       t.string :email, null: false
-      t.string :username, null: false
+      t.citext :username, null: false
       t.string :password_digest, null: false
       t.string :session_token, null: false
       t.string :first_name, null: false
       t.string :last_name, null: false
-      t.boolean :is_verified, 
+      t.boolean :is_verified, default: false, null: false
+    
 
       t.timestamps
     end
     add_index :users, :username, unique: true
+    add_index :users, :session_token, unique: true
   end
 end
