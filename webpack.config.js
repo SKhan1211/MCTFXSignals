cc5c253ea7e4ca0bd1548a81e898a8ce68b5a045
Suhaib <suhaib92khan@gmail.com>
2020-05-08 23:45:28 -0400

    Add users migration

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 module.exports = {
   context: __dirname,
-  entry: "./frontend/mct_fx_signals.jsx",
+  entry: "./client/mct_fx_signals.jsx",
   output: {
     path: path.resolve(__dirname, "app", "assets", "javascripts"),
     filename: "bundle.js",
