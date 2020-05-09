01d66d258add0bf81e74a1be8ff9127db4d47825
Suhaib <suhaib92khan@gmail.com>
2020-05-08 23:29:52 -0400

    Add DOMContentLoaded event listener to main React App file

---
+++ client/mct_fx_signals.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener("DOMContentLoaded", () => {
+  const root = document.getElementById("root");
+  ReactDOM.render(<h1>Welcome to MCT FX Signals</h1>, root);
+});
