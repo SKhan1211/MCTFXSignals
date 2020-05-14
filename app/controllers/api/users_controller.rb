995d5438b8681fe04f7529a1efea48ee22a6c968
Suhaib <suhaib92khan@gmail.com>
2020-05-13 21:58:17 -0400

    Add permit params for users controller

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,2 +1,19 @@
 class Api::UsersController < ApplicationController
+  def create
+    @user = User.new(user_params)
+  end
+  
+  def show
+  end
+  
+  def update
+  end
+  
+  def destroy
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :username, :password, :first_name, :last_name)
+  end
 end
