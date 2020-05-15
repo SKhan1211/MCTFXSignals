5bea1dce05e9b3e1eb61998a9b0ed2a9d489e305
Suhaib <suhaib92khan@gmail.com>
2020-05-15 18:50:52 -0400

    Create app and root components

---
+++ client/components/app.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => (
+  <div>
+    <h1>MCT FX Signals</h1>
+  </div>
+);
+
+export default App;
--- client/mct_fx_signals.jsx
+++ client/mct_fx_signals.jsx
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 
+import Root from './components/root';
+
 document.addEventListener("DOMContentLoaded", () => {
   const store = configureStore();
+
+  // TESTING START
   window.getState = store.getState;
   window.dispatch = store.dispatch;
+  // TESTING END
 
   const root = document.getElementById("root");
-  ReactDOM.render(<h1>Welcome to MCT FX Signals</h1>, root);
+  ReactDOM.render(<Root store={store} />, root);
 });
