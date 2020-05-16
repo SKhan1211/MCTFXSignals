81bac41d025c1685b40cec8f4d8147fe99169b19
Suhaib <suhaib92khan@gmail.com>
2020-05-16 17:20:50 -0400

    Add splash component

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
+import { AuthRoute, ProtectedRoute } from '../util/route_util';
+
 import NavbarContainer from './navbar/navbar_container';
 import SignUpFormContainer from './session_form/signup_form_container';
 import LogInFormContainer from './session_form/login_form_ container';
-import { AuthRoute, ProtectedRoute } from '../util/route_util';
+import Splash from './splash/splash';
 
 const App = () => (
   <div>
@@ -13,9 +15,9 @@ const App = () => (
       <NavbarContainer />
     </header>
     <Switch>
+      <Route exact path="/" component={Splash} />
       <AuthRoute path="/login" component={LogInFormContainer} />
       <AuthRoute path="/signup" component={SignUpFormContainer} />
-      {/* <Route exact path="/" component={} /> */}
     </Switch>
   </div>
 );
---
+++ client/components/splash/splash.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+
+const Splash = () => {
+
+  return (
+    <h1>I'm the splash component</h1>
+  )
+};
+
+export default Splash;
