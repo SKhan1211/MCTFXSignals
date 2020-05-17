628bf28a2f7acdf26b7d790b433dd23712449843
Suhaib <suhaib92khan@gmail.com>
2020-05-16 21:10:04 -0400

    add styling for navbar

--- client/components/splash/splash.jsx
+++ client/components/splash/splash.jsx
@@ -4,9 +4,20 @@ const Splash = () => {
 
   return (
     <div>
-      <section className="splash-section-header"></section>
-      <section className="splash-section-services"></section>
-      <section className="splash-section-whyus"></section>
+      <section className="splash-section-header container">
+        <nav>
+          <div className="splash-section-header nav__left-side">
+            <i class="fas fa-bars"></i>
+            <div className="PLACEHOLDER">LOGO</div> {/* Logo placeholder */}
+          </div>
+          <div className="splash-section-header nav__right-side">
+            <button className="splash-section-header nav__right-side__sign-in">Sign In</button>
+            <button className="splash-section-header nav__right-side__join-us">Join Us</button>
+          </div>
+        </nav>
+      </section>
+      <section className="splash-section-services container"></section>
+      <section className="splash-section-whyus container"></section>
     </div>
   )
 };
